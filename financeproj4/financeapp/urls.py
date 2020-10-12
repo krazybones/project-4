b975e9ac4a0039fe131e9762f7cba3eef221df76
krazybones <mbonuray@yahoo.com>
2020-10-12 15:49:09 -0500

    Added Stock Portfolio Info + css
    Added ability to use API to call different stocks + added bootstrap columns to dashboard

--- financeproj4/financeapp/urls.py
+++ financeproj4/financeapp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register')
 ]
--- financeproj4/financeapp/views.py
+++ financeproj4/financeapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -11,13 +15,64 @@ def home(request):
     return render(request, 'financeapp/home.html')
 
 
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'financeapp/dashboard.html')
+    import requests
+    import json
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+
+        # pk_d2c4a815f6d34f34bffd2b1760bd99e5
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_d2c4a815f6d34f34bffd2b1760bd99e5")
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'financeapp/dashboard.html', {'api': api})
+
+    else:
+        return render(request, 'financeapp/dashboard.html', {'ticker': "Enter a Ticker Symbol Above"})
 
 
 def register(request):
-    return render(request, 'financeapp/register.html')
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account created for ' + user)
+
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'financeapp/register.html', context)
 
 
-def login(request):
-    return render(request, 'financeapp/login.html')
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        return render(request, 'financeapp/login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
