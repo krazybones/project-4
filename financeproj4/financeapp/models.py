9a30d72911951e3c20cf40d64a36ac31ac821a79
krazybones <mbonuray@yahoo.com>
2020-10-14 00:49:41 -0500

    Add user profiles
    Added profile model for user profile capability

--- financeproj4/financeapp/models.py
+++ financeproj4/financeapp/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # pass account balance and transaction history here? i.e:
+    # balance = models.IntegerField()
+
+    def __str__(self):
+        return 'self.user.username Profile'
 
 
 class Stock(models.Model):
